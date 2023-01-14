465a16e690773d24416adc70ec22a36d23c9a6ff
Jacob Rose <jacob.r.rose96@gmail.com>
2023-01-14 14:31:59 -0500

    removed firebaseConfig; project complete

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from "./router"
 
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyCqYcYlo6loeCYPYgU3NiVVFFnLRdAFiG0",
-  authDomain: "p90x-entry.firebaseapp.com",
-  projectId: "p90x-entry",
-  storageBucket: "p90x-entry.appspot.com",
-  messagingSenderId: "227402872351",
-  appId: "1:227402872351:web:b3c9a013894c09cf0e072a",
-  measurementId: "G-T496BD2QKB"
+
+  
+  // place your firebase config here...
+
+
 };
 initializeApp(firebaseConfig);
 
