f63caa1e4b47e12ca05ec9d0883fc3638cdd373a
Jacob Rose <jacob.r.rose96@gmail.com>
2023-01-14 12:49:42 -0500

    built a working user registration view using firebase/auth library

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,1 +1,38 @@
-<template>Register</template>
+<template>
+  <h1>Create an account</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Password" v-model="password"></p>
+  <p><button @click="register">Submit</button></p>
+  <p><button @click="signInWithGoogle">Sign In With Google</button></p>
+</template>
+
+
+
+<script setup>
+import { ref } from "vue"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { useRouter } from 'vue-router' // import router
+
+const email = ref("")
+const password = ref("")
+const router = useRouter() // get a reference to our vue router
+
+const register = () => {
+  // need .value because ref()
+  const auth = getAuth() // from firebase/auth
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("Successfully registered!")
+      console.log(auth.currentUser)
+      router.push("/feed")
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    })
+}
+
+const signInWithGoogle = () => {
+
+}
+</script>
